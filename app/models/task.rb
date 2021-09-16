cf793a044ee0120fe58c0160c2c052845399de04
Martin Lopez <martin.lopezmoguel@gmail.com>
2021-09-16 17:19:58 -0500

    Añadiendo Participantes a las tareas

--- app/models/task.rb
+++ app/models/task.rb
@@ -13,7 +13,10 @@
 class Task < ApplicationRecord
   belongs_to :category
   belongs_to :owner, class_name: 'User'
-
+  has_many :paticipating_users, class_name: 'Participant'
+  has_many :participants, through: :participating_users, source: :user
+  
+  validates :participating_users, presence: true
   validates :name, :description, presence: true 
   validates :name, uniqueness: { case_insensitive: false }
   validate :due_date_validity
