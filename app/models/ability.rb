0dd684a6f89393fd155da2e9736db5095c545664
Martin Lopez <martin.lopezmoguel@gmail.com>
2021-09-27 16:24:12 -0500

    Añadiendo notas a las tareas con AJAX

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,5 +7,10 @@ class Ability
     
     can :manage, Task, owner_id: user.id 
     can :read, Task, participating_users: {user_id: user.id}
+    can :create, Note do |n|
+      owner_id = Task.find(n.task_id).owner_id
+      user_id = user.id
+      owner_id == user_id
+    end
   end
 end
